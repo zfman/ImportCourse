7280eaafcea929aee7d01901b1105b2751378953
zfman <13937267417@139.com>
2020-12-06 16:11:02 +0800

    v2.2.2

--- importcourselib/src/main/java/com/zhuangfei/adapterlib/AdapterLibManager.java
+++ importcourselib/src/main/java/com/zhuangfei/adapterlib/AdapterLibManager.java
@@ -19,8 +19,8 @@ public class AdapterLibManager {
     private static final String TAG = "AdapterLibManager";
 
     //核心库版本号
-    private static int libVersionNumber=22;
-    private static String libVersionName="lib-2.2.0";
+    private static int libVersionNumber=24;
+    private static String libVersionName="lib-2.2.2";
 
     public static int appVersionNumber=64;
 
